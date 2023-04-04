1eaec7ae6486d2a056aa4e7127dd040fc43b61c7
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2023-04-04 16:03:31 +0530

    refactored the code and fixed TS errors

--- config/wdio.android.conf.ts
+++ config/wdio.android.conf.ts
@@ -16,10 +16,7 @@ config.capabilities = [
         // The defaults you need to have in your config
         platformName: 'Android',
         maxInstances: 1,
-        // For W3C the appium capabilities need to have an extension prefix
-        // http://appium.io/docs/en/writing-running-appium/caps/
-        // This is `appium:` for all Appium Capabilities which can be found here
-        'appium:deviceName': 'Pixel_2_API_33',
+        'appium:deviceName': 'Pixel_XL_API_33',
         'appium:platformVersion': '13.0',
         'appium:orientation': 'PORTRAIT',
         'appium:automationName': 'UiAutomator2',
@@ -27,8 +24,6 @@ config.capabilities = [
         'appium:app': join(process.cwd(), './apps/Android-NativeDemoApp-0.4.0.apk'),
         // @ts-ignore
         'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
-        // Read the reset strategies very well, they differ per platform, see
-        // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         'appium:noReset': false,
         'appium:newCommandTimeout': 240,
     },
